b32b45263868fc85858dbdd1ed862aba80a28f20
Kaue Braga <kauebraga90@gmail.com>
2021-05-11 22:01:57 -0300

    update scripts 1.1 and 1.2

--- 1.1-monitoramento_frota.R
+++ 1.1-monitoramento_frota.R
@@ -1,9 +1,123 @@
+library(dplyr)
+library(data.table)
+library(ggplot2)
 
 # 1) Frota operante por linha pelo GPS para certas linhas por dia (+ gráfico) -----------------
+
+# 1.1) Abrir arquivo de GPS
+gps <- fread("gps_rio_amostra.csv")
+
+# 1.2) Selecionar e renomear colunas necessarias
+# verificar as colunas
+colnames(gps)
+
+gps_colunas <- gps %>% 
+  select(datahora, veiculo_id = ordem, linha, lon = longitude, lat = latitude)
+
+# 1.2) Extrair o dia e a hora
+# a coluna de datahora contem o dia e a hora no formato 2019-07-29 00:00:01
+# para facilitar as operacoes, vamos extrair o dia e a o momento do registro e criar 1 coluna para cada
+gps_colunas <- gps_colunas %>%
+  # criar as colunas de dia e hora
+  mutate(dia = format(datahora, "%d/%m/%y"),
+         momento = format(datahora, "%H:%M:%S"))
+
+# 1.2) Calcular a quantidade de pontos de GPS por dia, veiculo e linha
+gps_veiculos_linhas_pontos <- gps_colunas %>%
+  # agrupar por dia, veiculo e linha
+  group_by(dia, veiculo_id, linha) %>%
+  # calcular a quantidade de pontos por veiculo e linha
+  summarise(pontos_n = n()) %>%
+  ungroup()
+  
+  
+# 1.3) Calcular a quantidade de veiculos por linha
+# ao final, cada obsercao 
+gps_veiculos_linhas <-  gps_veiculos_linhas_pontos %>%
+  # agrupar por dia e linha
+  group_by(dia, linha) %>%
+  # calcular a quantidade de veiculos por linha
+  summarise(veiculos_n = n()) %>%
+  ungroup()
+
+# 1.4) Plot
+
+# iniciar o ggplot
+ggplot() +
+  # criar o grafico de barras
+  geom_col(data = gps_veiculos_linhas, aes(x = as.factor(linha), y = veiculos_n)) +
+  # ajustar texto
+  labs(x = "Linha", y = "Veiculos", title = "Quantidade de veiculos, por dia")
+
+# salvar o plot
+ggsave("plot1.png")
+
+# 1.5) Salvar os dados
+fwrite(gps_veiculos_linhas, "rio_veiculos_linhas.csv")
+
+
+# bonus: calcular quantidade de veiculos por linha e hora
+gps_colunas_hora <- gps_colunas %>%
+  # calcular a hora de cada registro
+  mutate(hora = format(datahora, "%H")) %>%
+  # agrupar por dia, veiculo e linha
+  group_by(dia, hora, veiculo_id, linha) %>%
+  # calcular a quantidade de pontos por veiculo e linha
+  summarise(pontos_n = n()) %>%
+  ungroup() %>%
+  # agrupar e contar agora por dia, hora e linha p/ calcular veciulos por hora
+  count(dia, hora, linha)
+
 
 
 
 # 2) Comparar com a frota que era pra estar rodando (left_join) (+ gráfico) -------------------
+
+# 2.1) Abrir dados com a quantidade de veiculos por linha
+veiculos_linhas <- fread("rio_veiculos_linhas.csv")
+
+# transformar a coluna de linha para character
+veiculos_linhas <- veiculos_linhas %>%
+  # reescrever a coluna com o mesmo nome
+  mutate(linha = as.character(linha))
+
+# 2.2) Abrir dados com a frota determinada
+frota_onibus <- fread("../../data-raw/curso_r_transportes/tabela_frota_rio.csv")
+
+# 2.3) Filtrar a frota somente das linhas qque estamos analisando
+frota_onibus_filt <- frota_onibus %>%
+  # fazer a selecao
+  # o codigo %in% faz a selecao de varios itens em um vetor
+  filter(linha_codigo %in% veiculos_linhas$linha)
+  
+
+# 2.3) Trazer a coluna da quantidade de veiculos da base de GPS p/ a base de frotas
+# a funcao lef_join vai manter todas as observaoes do lado esquerdo da juncao
+frota_onibus_gps <- left_join(
+  # base dados do lado esquerdo
+  x = frota_onibus_filt,
+  # base de dados do lado direito
+  y = veiculos_linhas,
+  # escolher a coluna que vai servir para relacionar as bases de dados
+  # como as colunas tem nome diferente, isso precisa estar explicito na seguinte forma
+  by = c("linha_codigo" = "linha")
+  
+)
+
+# 2.4) Plot
+
+# primeiro, calcular a diferenca abs entre a frota prevista e operada
+frota_onibus_gps <- frota_onibus_gps %>%
+  mutate(frota_dif = veiculos_n - frota_determinada)
+
+# iniciar ggplot
+ggplot() +
+  # grafico de barras com a quantidade de frota prevista e operada
+  geom_col(data = frota_onibus_gps, aes(x = linha_codigo, y = frota_dif, fill = frota_dif > 0))+
+  # ajustar texto
+  labs(x = "Linha", y = "Veiculos", title = "Diferenca absoluta entre veiculos operados e previstos, por dia")
+
+
 
 
 
--- 1.2-monitoramento_espacial_linhas.R
+++ 1.2-monitoramento_espacial_linhas.R
@@ -1,12 +1,56 @@
-
+library(dplyr) # manipulacao de dados
+library(data.table) # abrir e salvar dados (por enquanto)
+library(ggplot2) # graficos e mapas
+library(mapview) # visualizacao de dados espaciais
+library(sf) # operacoes com dados espaciais
 
 
 
 # 1) Verificação de itinerários praticados por linha via GPS a partir de shp do que es --------
 # Mapa com sobreposição dos pontos de GPS da linha com o shape da linha;
 
+# 1.1) Abrir arquivo de GPS
+gps <- fread("gps_rio_amostra.csv")
 
+# 1.2) Filtrar uma linha para o horario de 6 da manha
+gps_linha <- gps %>%
+  # extrair a hora do registro de GPS
+  mutate(hora = format(datahora, "%H")) %>%
+  # filtrar somente os registros de 06h
+  filter(hora == "06") %>%
+  # filtrar somente al inha 864
+  filter(linha == "864")
 
+# 1.3) Abrir arquivo com as linhas
+linhas_shape <- st_read("data-raw/2020-ago-30/2020-ago-30.shp")
+head(linhas_shape)
+
+# 1.4) Selecionar colunas necessarias
+linhas_shape_select <- linhas_shape %>%
+  # selecionar a coluna 'ref' e renomea-la para 'linha'
+  select(linha = ref, name)
+
+# 1.5) Selecionar linha de interesse
+linhas_shape_filter <- linhas_shape_select %>%
+  filter(linha == "864")
+
+# 1.6) Criar buffer em torno da linha para facilitar visualizacao
+linhas_shape_filter_buffer <- st_buffer(linhas_shape_filter, dist = 0.0005)
+
+# 1.6) Transformar os dados de GPS para formato espacial sf
+# isos vai permitir fazer a visualizacao espacial desses dados
+
+# primeiro, trocar a , por . nas coordenadas
+gps_linha_coords <- gps_linha %>%
+  # fazer a substituicao e salvar numa nova coluna
+  mutate(lon = sub(pattern = ",", replacement = ".", x = longitude),
+         lat = sub(pattern = ",", replacement = ".", x = latitude))
+
+# agora fazer a transformacao para sf
+gps_linha_sf <- st_as_sf(gps_linha_coords, coords = c("lon", "lat"), crs = 4326)
+
+# visualizar
+mapview(gps_linha_sf) + mapview(linhas_shape_filter_buffer)
 
 
 # 2) Embarque no BRT por estação e vans por hora (+ mapas e gráficos) -------------------------

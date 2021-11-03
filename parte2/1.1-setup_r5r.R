a341c20b3b0b67dc647c13f26c86abe641db9768
Kaue Braga <kauebraga90@gmail.com>
2021-11-03 17:55:59 -0300

    add new scripts

---
+++ parte2/1.1-setup_r5r.R
@@ -0,0 +1,5 @@
+# Esse script faz o setup das pastas e arquivos para a utilizacao do r5r para calculo de matriz
+# de tempo de viagem
+
+library(osmextract)
+library(sf)
---
+++ parte2/1.2-calcular_matriz.R
@@ -0,0 +1,19 @@
+# Esse script faz o calculo de uma matriz de tempo de viagem a partir de pontos OD e de um
+# arquivo de GTFS
+
+# carregar bibliotecas
+library(dplyr)
+library(r5r)
+
+# 1) Abrir arquivos ------------------
+
+
+
+# 2) Rodar ttmatrix ---------------------------------------------------------------------------
+
+
+
+
+# 3) Salvar -----------------------------------------------------------------------------------
+
+
